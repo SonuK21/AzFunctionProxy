a11218572ad9e5b923030c76ea0728d7dba3ea9c
Sonu Kumar <Sonu.Kumar@condecosoftware.com>
2020-02-08 14:35:23 +0530

    Added Todo API

---
+++ Models.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace AzFunctionProxy
+{
+    public class Todo
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("n");
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    }
+    public class TodoEntity : TableEntity
+    {
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
